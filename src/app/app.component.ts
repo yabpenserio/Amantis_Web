aee369bd65717cec2cce7e895bf8aa7804aae5c0
yabpenserio <yabpenserio@gmail.com>
2024-02-01 22:01:58 +0000

    Routing Added and corrected footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AmantisLandingPageComponent } from './amantis-landing-page/amantis-landing-page.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 import { AmantisWebHeaderComponent } from './amantis-web-header/amantis-web-header.component';
 import { AmantisWebFooterComponent } from './amantis-web-footer/amantis-web-footer.component';
-import { AmantisPcPageComponent } from './amantis-pc-page/amantis-pc-page.component';
-import { AmantisSoftwarePageComponent } from './amantis-software-page/amantis-software-page.component';
-import { AmantisContactPageComponent } from './amantis-contact-page/amantis-contact-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AmantisLandingPageComponent, AmantisWebHeaderComponent, AmantisWebFooterComponent, AmantisPcPageComponent,AmantisSoftwarePageComponent,AmantisContactPageComponent],
+  imports: [RouterOutlet, AmantisWebHeaderComponent, AmantisWebFooterComponent,RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AmantisLandingPageComponent } from './amantis-landing-page/amantis-landing-page.component';
+import { AmantisSoftwarePageComponent } from './amantis-software-page/amantis-software-page.component';
+import { AmantisPcPageComponent } from './amantis-pc-page/amantis-pc-page.component';
+import { AmantisContactPageComponent } from './amantis-contact-page/amantis-contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'landing_page', component: AmantisLandingPageComponent },
+    { path: 'software_page', component: AmantisSoftwarePageComponent },
+    { path: 'pc_page', component: AmantisPcPageComponent },
+    { path: 'contact_page', component: AmantisContactPageComponent },
+    { path: '**', component: AmantisLandingPageComponent },
+];
