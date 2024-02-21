16c376aa1dd103fc8a1997613cf5c4dc54afcdb2
yabpenserio <yabpenserio@gmail.com>
2024-02-21 00:16:55 +0000

    Correciones responsive

--- src/app/amantis-web-header/amantis-web-header.component.ts
+++ src/app/amantis-web-header/amantis-web-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-amantis-web-header',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './amantis-web-header.component.html',
   styleUrl: './amantis-web-header.component.css'
 })
-export class AmantisWebHeaderComponent {
+export class AmantisWebHeaderComponent implements OnInit {
+  public active : boolean = false 
+
+  constructor() { }
+  ngOnInit(): void {}
+
+  setActive() : void {
+    this.active = !this.active
+  }
 
 }
